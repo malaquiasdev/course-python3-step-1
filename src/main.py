c72ae9e4f596fa9509415f598e586ffe871bcfb6
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2019-02-03 00:43:59 +0000

    Created a new flux to help the user if the choise

--- src/main.py
+++ src/main.py
@@ -1,16 +1,26 @@
 print("**********************************************************")
 print(" Listen me. I want to play a game")
 print(" You need choice between the numbers 0 and 100")
-print(" Live or die, make your choice")
+print(" Make your choice !!!")
 print("**********************************************************")
 
 secret_number = 42
 
 user_input = int(input("Digit a number: "))
 
-if(user_input == secret_number):
+is_equal_than_secret_number = user_input == secret_number
+is_greater_than_secret_number= user_input > secret_number
+is_smaller_than_secret_number = user_input < secret_number
+
+if(is_equal_than_secret_number):
     print("Congratz... You are the winner!")
 else:
-    print("HAHAHAHA... You lose!!!")
+    if(is_greater_than_secret_number):
+        print("You missed! Your number is greater than the secret number!")
+    elif(is_smaller_than_secret_number):
+        print("You missed! Your number is smaller than the secret number!")
+
+
+#print("HAHAHAHA... You lose!!!")
 
 print("End game!")
