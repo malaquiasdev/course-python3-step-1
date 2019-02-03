924a5572577d03e860d69b3a5c399e817d376349
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2019-02-03 00:16:02 +0000

    Created the first flux of the game

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
-print("Listen me. I need you to cooperate with me, let's play?")
+print("**********************************************************")
+print(" Listen me. I need you to cooperate with me, let's play?")
+print("**********************************************************")
+
+secret_number = 42
+
+user_input = int(input("Digit a number: "))
+
+if(user_input == secret_number):
+    print("Congratz... You are the winner!")
+else:
+    print("HAHAHAHA... You lose!!!")
