aa5b23dfb02894d480cec393f2b0b39d9f1e2e79
Mateus Malaquias <mateus.malaquias@globalhitss.com.br>
2019-02-07 18:36:44 -0200

    Created a loop to make a better interative game

--- src/main.py
+++ src/main.py
@@ -5,22 +5,28 @@ print(" Make your choice !!!")
 print("**********************************************************")
 
 secret_number = 42
+attempts_total = 3
 
-user_input = int(input("Digit a number: "))
+for attempt in range(1, attempts_total + 1):
+    print("Attempt {} of {}".format(attempt, attempts_total))
 
-is_equal_than_secret_number = user_input == secret_number
-is_greater_than_secret_number= user_input > secret_number
-is_smaller_than_secret_number = user_input < secret_number
+    user_input = int(input("Digit a number: "))
 
-if(is_equal_than_secret_number):
-    print("Congratz... You are the winner!")
-else:
-    if(is_greater_than_secret_number):
-        print("You missed! Your number is greater than the secret number!")
-    elif(is_smaller_than_secret_number):
-        print("You missed! Your number is smaller than the secret number!")
+    if(user_input < 1 or user_input > 100):
+        print("You must enter a number between 1 and 100!")
+        continue
 
+    is_equal_than_secret_number = user_input == secret_number
+    is_greater_than_secret_number= user_input > secret_number
+    is_smaller_than_secret_number = user_input < secret_number
 
-#print("HAHAHAHA... You lose!!!")
+    if(is_equal_than_secret_number):
+        print("Congratz... You are the winner!")
+        break
+    else:
+        if(is_greater_than_secret_number):
+            print("You missed! Your number is greater than the secret number!")
+        elif(is_smaller_than_secret_number):
+            print("You missed! Your number is smaller than the secret number!")
 
 print("End game!")
