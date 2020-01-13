226ad0902050afa2ff567f2739029c2f662f3182
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2020-01-13 20:43:23 -0300

    find the user input inside of the scret_word

--- src/hangman_game.py
+++ src/hangman_game.py
@@ -8,7 +8,15 @@ def play():
     win = False
 
     while(not win and not hanged):
-        print('Playing...')
+        user_input = input(" Tell me a letter, please? ")
+
+        index = 0
+        for letter in secret_word:
+            if(user_input.lower() == letter):
+                print(f"We found the letter {letter} in the position {index}")
+            index += 1
+
+        print(" Playing...")
 
     print("**********************************************************")
     print(" You lose! T.T ")
