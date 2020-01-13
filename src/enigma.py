04872dffe25c20acb43e05ec50273c94f5815e99
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2020-01-13 20:11:18 -0300

    created a new structure to put more games in this project

---
+++ src/enigma.py
@@ -0,0 +1,54 @@
+import random
+
+
+def play():
+    print("**********************************************************")
+    print(" Listen me. I want to play a game")
+    print(" You need choice between the numbers 0 and 100")
+    print(" Make your choice !!!")
+    print("**********************************************************")
+
+    secret_number = random.randrange(1, 101)
+    attempts_total = 0
+    user_points = 1000
+
+    print("Select a difficulty level for your match")
+    match_level = int(input("(1) Easy, (2) Medium, (3) Hard: "))
+
+    if(match_level == 1):
+        attempts_total = 20
+    elif(match_level == 2):
+        attempts_total = 10
+    else:
+        attempts_total = 5
+
+    for attempt in range(1, attempts_total + 1):
+        print(f"Attempt {attempt} of {attempts_total}")
+
+        user_input = int(input("Digit a number: "))
+
+        if(user_input < 1 or user_input > 100):
+            print("You must enter a number between 1 and 100!")
+            continue
+
+        is_equal_than_secret_number = user_input == secret_number
+        is_greater_than_secret_number = user_input > secret_number
+        is_smaller_than_secret_number = user_input < secret_number
+
+        if(is_equal_than_secret_number):
+            print(
+                f"Congratz... You are the winner! Your scored is {user_points}.")
+            break
+        else:
+            if(is_greater_than_secret_number):
+                print("You missed! Your number is greater than the secret number!")
+            elif(is_smaller_than_secret_number):
+                print("You missed! Your number is smaller than the secret number!")
+            lost_points = abs(secret_number - user_input)
+            user_points = user_points - lost_points
+
+    print("End game!")
+
+
+if(__name__ == "__main__"):
+    play()
--- src/main.py
+++ src/main.py
@@ -1,47 +1,23 @@
-import random
+import enigma
+import hangman_game
 
-print("**********************************************************")
-print(" Listen me. I want to play a game")
-print(" You need choice between the numbers 0 and 100")
-print(" Make your choice !!!")
-print("**********************************************************")
 
-secret_number = random.randrange(1, 101)
-attempts_total = 0
-user_points = 1000
+def choose_game():
+    print("*********************************")
+    print("******* Choose your game! *******")
+    print("*********************************")
 
-print("Select a difficulty level for your match")
-match_level = int(input("(1) Easy, (2) Medium, (3) Hard: "))
+    print("(1) Enigma (2) Hangman Game")
 
-if(match_level == 1):
-    attempts_total = 20
-elif(match_level == 2):
-    attempts_total = 10
-else:
-    attempts_total = 5
+    jogo = int(input(" What is your choice? "))
 
-for attempt in range(1, attempts_total + 1):
-    print(f"Attempt {attempt} of {attempts_total}")
+    if(jogo == 1):
+        print("Starting the enigma")
+        enigma.play()
+    elif(jogo == 2):
+        print("Starting the Hangman Game")
+        hangman_game.play()
 
-    user_input = int(input("Digit a number: "))
 
-    if(user_input < 1 or user_input > 100):
-        print("You must enter a number between 1 and 100!")
-        continue
-
-    is_equal_than_secret_number = user_input == secret_number
-    is_greater_than_secret_number= user_input > secret_number
-    is_smaller_than_secret_number = user_input < secret_number
-
-    if(is_equal_than_secret_number):
-        print(f"Congratz... You are the winner! Your scored is {user_points}.")
-        break
-    else:
-        if(is_greater_than_secret_number):
-            print("You missed! Your number is greater than the secret number!")
-        elif(is_smaller_than_secret_number):
-            print("You missed! Your number is smaller than the secret number!")
-        lost_points = abs(secret_number - user_input)
-        user_points = user_points - lost_points
-
-print("End game!")
+if(__name__ == "__main__"):
+    choose_game()
