3e6ccc0d7b2836783091e85cea91da4c65ca1678
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2020-01-13 20:24:41 -0300

    game loop

--- src/hangman_game.py
+++ src/hangman_game.py
@@ -3,6 +3,13 @@ def play():
     print(" Welcome to the Hangman Game u.u ")
     print("**********************************************************")
 
+    secret_word = "banana"
+    hanged = False
+    win = False
+
+    while(not win and not hanged):
+        print('Playing...')
+
     print("**********************************************************")
     print(" You lose! T.T ")
     print("**********************************************************")
