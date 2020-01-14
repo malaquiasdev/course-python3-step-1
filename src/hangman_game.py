de7bdf2b65b8d240cf9d738b878a4aabc7e4938a
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2020-01-13 21:01:23 -0300

    created a function to encapsulate the game loop flow

--- src/hangman_game.py
+++ src/hangman_game.py
@@ -1,3 +1,20 @@
+def user_input_is_equal_to_letter(user_input, letter):
+    return user_input.lower().strip() == letter.lower().strip()
+
+
+def game_loop(secret_word, hanged, win):
+    while(not win and not hanged):
+        user_input = input(" Tell me a letter, please? ")
+
+        index = 0
+        for letter in secret_word:
+            if(user_input_is_equal_to_letter(user_input, letter)):
+                print(f"We found the letter {letter} in the position {index}")
+            index += 1
+
+        print(" Playing...")
+
+
 def play():
     print("**********************************************************")
     print(" Welcome to the Hangman Game u.u ")
@@ -7,16 +24,7 @@ def play():
     hanged = False
     win = False
 
-    while(not win and not hanged):
-        user_input = input(" Tell me a letter, please? ")
-
-        index = 0
-        for letter in secret_word:
-            if(user_input.lower() == letter):
-                print(f"We found the letter {letter} in the position {index}")
-            index += 1
-
-        print(" Playing...")
+    game_loop(secret_word, hanged, win)
 
     print("**********************************************************")
     print(" You lose! T.T ")
