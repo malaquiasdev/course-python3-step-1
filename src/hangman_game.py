69c2a695573f1f394d4f1e81f90e75d639e88451
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2020-01-13 21:40:24 -0300

    saving the user hit letters in a list

--- src/hangman_game.py
+++ src/hangman_game.py
@@ -2,29 +2,29 @@ def user_input_is_equal_to_letter(user_input, letter):
     return user_input.lower().strip() == letter.lower().strip()
 
 
-def game_loop(secret_word, hanged, win):
+def game_loop(secret_word, user_hit_letters, hanged, win):
+    print(f'Hit letters {user_hit_letters}')
     while(not win and not hanged):
         user_input = input(" Tell me a letter, please? ")
-
         index = 0
         for letter in secret_word:
             if(user_input_is_equal_to_letter(user_input, letter)):
-                print(f"We found the letter {letter} in the position {index}")
+                user_hit_letters[index] = letter
             index += 1
-
-        print(" Playing...")
+        print(user_hit_letters)
 
 
 def play():
     print("**********************************************************")
     print(" Welcome to the Hangman Game u.u ")
     print("**********************************************************")
-
+    
     secret_word = "banana"
+    user_hit_letters = ["_", "_", "_", "_", "_", "_"]
     hanged = False
     win = False
 
-    game_loop(secret_word, hanged, win)
+    game_loop(secret_word, user_hit_letters, hanged, win)
 
     print("**********************************************************")
     print(" You lose! T.T ")
